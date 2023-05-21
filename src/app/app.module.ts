b9473471b99f9b0dcebc97e065403663e2e9ea50
Wagner Filho <wagner.santos@acto.com.br>
2023-05-21 20:21:03 +0100

    initial commit - esqueleto do blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './componentes/menu-title/menu-title.component';
+import { CardMainComponent } from './componentes/card-main/card-main.component';
+import { CardSmallComponent } from './componentes/card-small/card-small.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    CardMainComponent,
+    CardSmallComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/card-main/card-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-main',
+  templateUrl: './card-main.component.html',
+  styleUrls: ['./card-main.component.css']
+})
+export class CardMainComponent {
+
+}
