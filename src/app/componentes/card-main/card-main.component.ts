db5c664208e04a2196cdbda5c3bd323d5a9566aa
Wagner Filho <wagner.santos@acto.com.br>
2023-05-21 20:51:16 +0100

    desacoplar dados estaticos

--- src/app/componentes/card-main/card-main.component.ts
+++ src/app/componentes/card-main/card-main.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-main',
   templateUrl: './card-main.component.html',
   styleUrls: ['./card-main.component.css']
 })
-export class CardMainComponent {
+export class CardMainComponent implements OnInit{
+  @Input() imageSrc: string = "";
+  @Input() titulo: string = "";
+  @Input() descricao: string = "";
+
+  ngOnInit(): void {
+    //this.imageSrc = "../../../assets/img/card-main.png";
+    //this.titulo = "Titulo da Notícia Dinâmico";
+    //this.descricao = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+  }
 
 }
--- src/app/componentes/card-small/card-small.component.ts
+++ src/app/componentes/card-small/card-small.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-small',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-small.component.css']
 })
 export class CardSmallComponent {
-
+  @Input() data:string = "";
+  @Input() descricao:string = "";
+  @Input() imagem:string = "";
 }
